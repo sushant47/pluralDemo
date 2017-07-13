aafdaefe2cc7da89a5470074697db611c371e15c
sushant bilgi <sushant.bilgi@Indussoft.com>
2017-07-13 16:09:20 +0530

    first commit

---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import { EventsAppComponent } from "./events-app.component";
+import { EventsListComponent } from "./events/events-list.component";
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [EventsAppComponent, EventsListComponent],
+    bootstrap: [EventsAppComponent]
+})
+
+export class AppModule{}
